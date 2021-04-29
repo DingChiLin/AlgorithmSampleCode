0d20d33b11f3a7fb14271b9b99dbad291a30c5cc
Arthur Lin <gn01168178@yahoo.com.tw>
2021-04-29 10:48:04 +0800

    add Trapping Rain Water

---
+++ leetcode/TrappingRainWater.py
@@ -0,0 +1,14 @@
+class Solution:
+	def trap(self, height: List[int]) -> int:
+		stk = []
+		ans = 0
+		for i in range(len(height)):
+			while(len(stk) > 0 and height[i] > height[stk[-1]]):
+				if (len(stk) > 1):
+					w = i - stk[-2] - 1
+					h = min(height[i], height[stk[-2]]) - height[stk[-1]]
+					ans += w * h
+				stk.pop()
+			stk.append(i)
+
+		return ans
