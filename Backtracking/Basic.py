31633cb5121929ff58764e2dbcbfd12d401d6a7e
Arthur Lin <gn01168178@yahoo.com.tw>
2021-06-20 11:33:41 +0800

    modify backtracking basic

--- Backtracking/Basic.py
+++ Backtracking/Basic.py
@@ -8,38 +8,38 @@ def backtracking(n):
 
 backtracking(1)
 
-def backtracking_1(n, nums):
-    print(nums)
-    if n >= 2:
-        return
+# def backtracking_1(n, nums):
+#     print(nums)
+#     if n >= 2:
+#         return
 
-    new_nums = nums + [1]
-    backtracking_1(n+1, new_nums)
+#     new_nums = nums + [1]
+#     backtracking_1(n+1, new_nums)
 
-backtracking_1(0, [])
+# backtracking_1(0, [])
 
-def backtracking_2(n, nums):
-    print(nums)
-    if n >= 2:
-        return
+# def backtracking_2(n, nums):
+#     print(nums)
+#     if n >= 2:
+#         return
 
-    for i in range(1, 3): # 1, 2
-        new_nums = nums + [i]
-        backtracking_2(n + 1, new_nums)
+#     for i in range(1, 3): # 1, 2
+#         new_nums = nums + [i]
+#         backtracking_2(n + 1, new_nums)
 
-backtracking_2(0, [])
+# backtracking_2(0, [])
 
-def backtracking_2_b(n, nums):
-    print(nums)
-    if n >= 2:
-        return
+# def backtracking_2_b(n, nums):
+#     print(nums)
+#     if n >= 2:
+#         return
 
-    for i in range(1, 3): # 1, 2
-        nums.append(i)
-        backtracking_2_b(n + 1, nums)
-        nums.pop()
+#     for i in range(1, 3): # 1, 2
+#         nums.append(i)
+#         backtracking_2_b(n + 1, nums)
+#         nums.pop()
 
-backtracking_2_b(0, [])
+# backtracking_2_b(0, [])
 
 # def backtracking_3(n, nums):
 #     print(nums)
