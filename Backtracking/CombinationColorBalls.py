8594e4b50e9608a9ecc900444c06481d39660135
Arthur Lin <gn01168178@yahoo.com.tw>
2021-06-28 09:57:24 +0800

    add another way for combination color balls

--- Backtracking/CombinationColorBalls.py
+++ Backtracking/CombinationColorBalls.py
@@ -2,7 +2,30 @@
 
 from typing import List
 
+# Like N Digit Password
 class Solution:
+    def __init__(self):
+        self.ans = []
+    
+    def find(self, depth: int, max_depth: int, balls: List[int], k: int, comb: List[int]):
+        if depth == max_depth:
+            if k == 0:
+                self.ans.append(comb[:])
+            return
+
+        for i in range(balls[depth] + 1):
+            if i <= k:
+                comb.append(i)
+                self.find(depth + 1, max_depth, balls, k - i, comb)
+                comb.pop()
+
+    def colorBallsCombination(self, balls: List[int], k: int):
+        self.ans = []
+        self.find(0, len(balls), balls, k, [])
+        return self.ans
+
+# Add balls one by one
+class Solution2:
     def colorBallsCombination(self, balls: List[int], k: int) -> List[List[int]]:
         ans = []
 
