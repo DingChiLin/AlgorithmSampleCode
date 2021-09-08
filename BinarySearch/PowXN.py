431c5d6ed45df068ccd6466b61570fbf3bb3180b
Arthur Lin <gn01168178@yahoo.com.tw>
2021-09-08 21:57:22 +0800

    modify pow

--- BinarySearch/PowXN.py
+++ BinarySearch/PowXN.py
@@ -1,4 +1,18 @@
-# O(logN)
+# Exponentiating by squaring O(logN) 
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        sign = (n >= 0)
+        n = abs(n)
+        ans = 1
+        cur = x
+        while(n > 0):
+            if (n % 2 == 1):
+                ans *= cur
+            cur *= cur
+            n //= 2
+        return ans if sign else 1/ans
+
+# Exponentiating by squaring + bit operation O(logN) 
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         sign = (n >= 0)
