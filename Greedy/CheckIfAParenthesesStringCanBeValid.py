e07d4d3610646c354ef454617f963ae1e554e3b0
Arthur Lin <gn01168178@yahoo.com.tw>
2021-12-28 11:52:44 +0800

    add CheckIfAParenthesesStringCanBeValid.py

---
+++ Greedy/CheckIfAParenthesesStringCanBeValid.py
@@ -0,0 +1,42 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        N = len(s)
+        
+        # Count of '(', ')' and free positions
+        L, R, F = 0, 0, 0
+        for i in range(N):
+            if locked[i] == '0':
+                F += 1
+            else:
+                if s[i] == '(':
+                    L += 1
+                else:
+                    R += 1
+        FL2 = (F + R - L)
+        if (FL2 & 1): 
+            # It's impossible make the total count of '(' and ')' balanced by using free positions
+            return False
+
+        # how many free position should become '(', the others will become ')'
+        FL = FL2 // 2
+
+        # Greedy: if we can choose some free position to be '(', the sooner the better
+        stk = 0
+        for i in range(N):
+            if locked[i] == '0':
+                if FL > 0:
+                    stk += 1
+                    FL -= 1
+                else:
+                    stk -= 1
+            else:
+                if s[i] == '(':
+                    stk += 1
+                else:
+                    stk -= 1
+            if stk < 0:
+                return False
+        return stk == 0
+                
+            
+        
